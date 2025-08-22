12908d559f6b700726669e0192cf4ba68d3a768b
sumatinikam1999 <125386670+sumatinikam1999@users.noreply.github.com>
2025-08-22 17:52:04 +0000

    addded

--- AWS/session-7/amazon-infra/vpc.tf
+++ AWS/session-7/amazon-infra/vpc.tf
@@ -1,3 +1,9 @@
 module "vpc" {
-  source = "git::"
+  source = "git::https://github.com/sumatinikam1999/terraform-aws-vpc-module-dev.git"
+  project_name = var.project_name
+  cidr_block = var.cidr_block
+  common_tags = var.common_tags
+  public_subnet_cidr_block = var.public_subnet_cidr_block
+  private_subnet_cidr_block = var.private_subnet_cidr_block
+  database_subnet_cidr_block = var.database_subnet_cidr_block
 }
