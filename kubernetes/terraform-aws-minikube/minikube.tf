1f13e6dc5682ea5011004fec602132b0408068d9
sumatinikam1999 <125386670+sumatinikam1999@users.noreply.github.com>
2025-09-05 13:16:28 +0530

    added

---
+++ kubernetes/terraform-aws-minikube/minikube.tf
@@ -0,0 +1,23 @@
+module "minikube" {
+  source = "github.com/scholzj/terraform-aws-minikube"
+
+  aws_region    = "us-east-1"
+  cluster_name  = "my-minikube"
+  aws_instance_type = "t2.medium"
+  ssh_public_key = "~/.ssh/id_rsa.pub"
+  aws_subnet_id = "subnet-8a3517f8"
+  ami_image_id = "ami-b81dbfc5"
+  hosted_zone = "my-domain.com"
+  hosted_zone_private = false
+
+  tags = {
+    Application = "Minikube"
+  }
+
+  addons = [
+      "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/kubernetes-dashabord/init.sh",
+      "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/kubernetes-metrics-server/init.sh",
+      "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/external-dns/init.sh",
+      "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/kubernetes-nginx-ingress/init.sh"
+  ]
+}
